99477b44d0275e031f945387f904f6a8b4dfd8e4
LukasLow <zoll.radium-0b@icloud.com>
2023-07-07 15:04:15 +0000

    Build: ()

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '239046766494320721409577827159647459535'                       // used to check whether the localStorage data is stale
+var gzip_hash = '226984814351791357086025397133055310593'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
